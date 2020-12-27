bb59275b58307e3c47b993a24efa4b01944f61c6
Christer Andersson <christer.andersson@intito.se>
2020-12-27 20:02:24 +0100

    Initial commits...

---
+++ ccr_64/DbConv.bat
@@ -0,0 +1,2 @@
+:: Slightly shorter than the official TN; don't forget adjusting the install path:
+"C:\app\ccr_64\bin64\jre\8.0\bin\javaw.exe" -Dcom.ibm.jsse2.overrideDefaultTLS=true -Dcom.ibm.jsse2.overrideDefaultProtocol=TLSv12 -cp "C:\app\ccr_64\DbConv.jar";"C:\app\ccr_64\server\integration\*" com.ibm.cognos.ccr.dbconv.ui.DbConvGUI
